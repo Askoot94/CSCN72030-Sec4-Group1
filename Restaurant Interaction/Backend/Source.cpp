c834fc47aae4350452facaa6654cf86c87ab11d7
Askoot94 <98133009+Askoot94@users.noreply.github.com>
2023-10-31 18:19:55 -0400

    + Prepared Canvas Template for Individual Branches

---
+++ Restaurant Interaction/Backend/Source.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+/*
+This is the main back bone for our Visual studios project. This project will have all of the background classes and functions needed for the functionality
+of our system.
+
+
+*/
+
+int main(void) {
+
+	//Initialize the entire system. 
+
+	//Choose what type of interface display this device will before. This is choosing the account permission for this interface
+
+		//Initialize If for Staff
+
+		//Initialize If for Customers
+
+	//Create Order Objects and Recieve Order information for the object
+
+	//Updates to Order or Dish Statuses
+
+	//Update any Assistance Requests
+
+	//Dispose of Order Objects
+
+	//Turn Off Display
+	return 0;
+};
