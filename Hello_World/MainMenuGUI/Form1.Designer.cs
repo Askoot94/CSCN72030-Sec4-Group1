a97ab1c647936e93c9823201d9b1d6e010662eed
Yinus Alamutu <Yalamutu2169@conestogac.on.ca>
2023-11-16 10:24:46 -0500

    Increase and Decrease button in Main Menu

--- Hello_World/MainMenuGUI/Form1.Designer.cs
+++ Hello_World/MainMenuGUI/Form1.Designer.cs
@@ -32,6 +32,15 @@
             this.button3 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.button12 = new System.Windows.Forms.Button();
+            this.button13 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
@@ -43,41 +52,145 @@
             this.button1.TabIndex = 1;
             this.button1.Text = "Ethan\'s Restaurant";
             this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button3
             // 
+            this.button3.BackColor = System.Drawing.SystemColors.ControlDark;
             this.button3.Location = new System.Drawing.Point(353, 93);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(230, 53);
             this.button3.TabIndex = 3;
             this.button3.Text = "Glutten Free Dinner";
-            this.button3.UseVisualStyleBackColor = true;
+            this.button3.UseVisualStyleBackColor = false;
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(12, 93);
+            this.button2.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.button2.Location = new System.Drawing.Point(1, 93);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(230, 53);
             this.button2.TabIndex = 4;
             this.button2.Text = "Veg Dinner";
-            this.button2.UseVisualStyleBackColor = true;
+            this.button2.UseVisualStyleBackColor = false;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button4
             // 
+            this.button4.BackColor = System.Drawing.SystemColors.ControlDarkDark;
             this.button4.Location = new System.Drawing.Point(758, 102);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(230, 53);
             this.button4.TabIndex = 5;
-            this.button4.Text = "button4";
-            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Text = "Diary Free";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.button5.Location = new System.Drawing.Point(1, 162);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(288, 49);
+            this.button5.TabIndex = 6;
+            this.button5.Text = "Search for food";
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.button6.Location = new System.Drawing.Point(1, 235);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(150, 36);
+            this.button6.TabIndex = 7;
+            this.button6.Text = "Popular";
+            this.button6.UseVisualStyleBackColor = false;
+            // 
+            // button7
+            // 
+            this.button7.BackgroundImage = global::MainMenuGUI.Properties.Resources.Screenshot_2023_11_08_131604;
+            this.button7.Location = new System.Drawing.Point(1, 294);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(211, 268);
+            this.button7.TabIndex = 8;
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.BackgroundImage = global::MainMenuGUI.Properties.Resources.Screenshot_2023_11_08_131141;
+            this.button8.Location = new System.Drawing.Point(370, 294);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(230, 268);
+            this.button8.TabIndex = 9;
+            this.button8.Text = "button8";
+            this.button8.UseVisualStyleBackColor = true;
+            // 
+            // button9
+            // 
+            this.button9.BackgroundImage = global::MainMenuGUI.Properties.Resources.Screenshot_2023_11_08_131307;
+            this.button9.Location = new System.Drawing.Point(768, 294);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(230, 268);
+            this.button9.TabIndex = 10;
+            this.button9.UseVisualStyleBackColor = true;
+            // 
+            // button10
+            // 
+            this.button10.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.button10.Location = new System.Drawing.Point(1, 558);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(211, 58);
+            this.button10.TabIndex = 11;
+            this.button10.Text = "Fried Chicken";
+            this.button10.UseVisualStyleBackColor = false;
+            // 
+            // button11
+            // 
+            this.button11.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.button11.Location = new System.Drawing.Point(370, 558);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(230, 58);
+            this.button11.TabIndex = 12;
+            this.button11.Text = "Burger";
+            this.button11.UseVisualStyleBackColor = false;
+            // 
+            // button12
+            // 
+            this.button12.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.button12.Location = new System.Drawing.Point(768, 558);
+            this.button12.Name = "button12";
+            this.button12.Size = new System.Drawing.Size(230, 58);
+            this.button12.TabIndex = 13;
+            this.button12.Text = "Sushi";
+            this.button12.UseVisualStyleBackColor = false;
+            // 
+            // button13
+            // 
+            this.button13.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.button13.Location = new System.Drawing.Point(779, 744);
+            this.button13.Name = "button13";
+            this.button13.Size = new System.Drawing.Size(219, 42);
+            this.button13.TabIndex = 14;
+            this.button13.Text = "Add to Cart";
+            this.button13.UseVisualStyleBackColor = false;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = global::MainMenuGUI.Properties.Resources.GUI_Main_Menu_Background;
-            this.ClientSize = new System.Drawing.Size(1000, 785);
+            this.ClientSize = new System.Drawing.Size(998, 785);
+            this.Controls.Add(this.button13);
+            this.Controls.Add(this.button12);
+            this.Controls.Add(this.button11);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button3);
@@ -95,6 +208,15 @@
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.Button button12;
+        private System.Windows.Forms.Button button13;
     }
 }
 
--- Hello_World/MainMenuGUI/Form1.cs
+++ Hello_World/MainMenuGUI/Form1.cs
@@ -26,5 +26,25 @@ namespace MainMenuGUI
         {
 
         }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
