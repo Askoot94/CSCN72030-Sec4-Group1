b7f89c700600339a10be59722d53d9d38accb0e9
Shy <85186628+Shy2003@users.noreply.github.com>
2023-12-08 17:22:21 -0500

    Added 12 pages MainMenu A1->C3

---
+++ [Project III]GUI/MainMenuC3.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _Project_III_GUI
+{
+    public partial class MainMenuC3 : Form
+    {
+        public string tableOrder;
+
+        public MainMenuC3(string table)
+        {
+            InitializeComponent();
+
+            tableOrder = table;
+        }
+
+
+
+        private void MainMenuC3_Load(object sender, EventArgs e)
+        {
+            label5.Text = tableOrder;
+        }
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
